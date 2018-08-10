38b0ff161361da0e33a257cfb74c2dfe7fccbd52
Jimmy Brannon <user.email>
2018-08-09 21:13:36 -0500

    create error middleware

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3060;
 
 const userRoutes = require('./app/users/routes');
+const errorRoute = require('./modules/error');
 
 app.use(bodyParser.json());
 
 app.use('/api/user', userRoutes);
+app.use(errorRoute);
 
 
 // Run server
---
+++ modules/error.js
@@ -0,0 +1,5 @@
+module.exports = (error, req, res, next) => {
+  res.status(error.status || 500).json({
+    message: error.message || 'Something went wrong'
+  });
+}
