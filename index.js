3d503b4f8c3c272d8a07dcc160a6ad726e57e415
Jimmy Brannon <user.email>
2018-08-12 13:43:09 -0500

    send 404 message

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3060;
 
 const userRoutes = require('./app/users/routes');
+const FOFRoute = require('./modules/FOFRoute');
 const errorRoute = require('./modules/error');
 
 // middleware
@@ -12,6 +13,9 @@ app.use(bodyParser.json());
 
 // my defined routes
 app.use('/api/user', userRoutes);
+
+// route to handle any 404s
+app.use(FOFRoute);
 
 // error route to handle all application errors
 app.use(errorRoute);
